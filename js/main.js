9605eb8907fa26640b9ccfcdeedc6db949981fc7
harakiyo <chihokiyohara1004@gmail.com>
2023-10-02 09:16:05 +0900

    PC完成版

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// $(function($){
+//     $('.slider').slick({
+//         autoplay: false, 
+//         arrows: true, 
+//         dots: true, 
+//         infinite: true, 
+//         slidesToScroll: 1, 
+//         slidesToShow: 4, /* 初期表示のスライド数を4つに設定 */
+//         responsive: [
+//             {
+//                 breakpoint: 1024, /* 横幅が1024px以下の場合の設定 */
+//                 settings: {
+//                     slidesToShow: 3 /* スライド数を3つに変更 */
+//                 }
+//             },
+//             {
+//                 breakpoint: 768, /* 横幅が768px以下の場合の設定 */
+//                 settings: {
+//                     slidesToShow: 2 /* スライド数を2つに変更 */
+//                 }
+//             },
+//             {
+//                 breakpoint: 450, /* 横幅が450px以下の場合の設定 */
+//                 settings: {
+//                     slidesToShow: 1 /* スライド数を1つに変更 */
+//                 }
+//             }
+//         ]
+//     });
+// });
